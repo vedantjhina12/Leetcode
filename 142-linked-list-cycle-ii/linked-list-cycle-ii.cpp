a8c7a4cccfc595ac07e79b6cb19e9520595f0da4
Vedant Jhina <vedantjhina2@gmail.com>
2026-05-05 20:38:43 +0530

    Time: 7 ms (65.43%) | Memory: 11.3 MB (85.24%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+
+public:
+    ListNode* floyd_detection(ListNode *head){
+         if (head == NULL)
+            return NULL;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (slow != NULL && fast != NULL && fast->next!=NULL) {
+            fast = fast->next;
+            if (fast != NULL)
+                fast = fast->next;
+            slow = slow->next;
+            if (slow == fast)
+                return slow;
+        }
+        return NULL;
+    }
+    ListNode *detectCycle(ListNode *head) {
+         if (head == NULL)
+            return NULL;
+        
+        ListNode* intersection=floyd_detection(head);
+        if(intersection==NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+
+        while(slow!=intersection){
+            slow=slow->next;
+            intersection=intersection->next;
+        }
+        return slow;
+    }
+};
